bc1d6773be677f7a268c5ca89683d07c95856dd3
Alexandre <caramel.alexandre@gmail.com>
2022-02-04 23:28:04 +0100

    ADD reset button for quizz

--- src/pages/QuizzOpen.js
+++ src/pages/QuizzOpen.js
@@ -1,5 +1,5 @@
 import "../Sass/components/_QuizzOpen.scss";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import quizzData from '../data/quizz.json'
 
 const QuizzOpen = (props) => {
@@ -7,36 +7,64 @@ const QuizzOpen = (props) => {
     const idQuizz = props.match.params.quizzId - 1
     // Recuperer les question en lien avec le Quizz
     const QuestionQuizz = quizzData.quizz[idQuizz].questions
-    
     const [count, setCount] = useState(0);
     const [score, setScore] = useState(0);
-    // Permet de vérifier la réponse en fonction de l'input séléctionné
-    const validation = () => {
-        if(count <= 8){
+
+    // Permets de mélanger les réponses
+    for(var i = 0; i < QuestionQuizz.length; i++){
+        QuestionQuizz[i].reponses.sort(() => Math.random() - 0.5);
+    }
+    useEffect(()=> {
+        // Enregistrer le score de l'utilisateur dans le localStorage
+        localStorage.setItem(quizzData.quizz[idQuizz].slug, score)
+    },[validation])
+
+    // Permet de vérifier la réponse en fonction de l'input sélectionné
+    function validation(){
+        console.log(count);
+        if(count < 9){
             const valueResponse = document.querySelector('input[name="quizz"]:checked').value
-            console.log(valueResponse);
             if(valueResponse === QuestionQuizz[count].answer){
                 setScore(score + 1)
+                console.log(score);
             }
             document.querySelector('input[name="quizz"]:checked').checked = false;
             setCount(count + 1)
         }else{
-            document.getElementById("submit").style.display = "none"
+            document.getElementById("QuizzEnCours").style.display = "none"
+            document.getElementById("QuizzTermine").style.display = "block"
         }
+    }
+
+    function relancer(){
+        // Permets de relancer le quizz une fois qu'il est terminé
+        document.getElementById("QuizzEnCours").style.display = "block"
+        document.getElementById("QuizzTermine").style.display = "none"
+        document.querySelector('input[name="quizz"]:checked').checked = false;
+        setCount(0)
+        setScore(0)
+        
     }
 
     return (
         <section id="QuizzOpen">
-            <h2>Score: {score} </h2>
-            {
-                    QuestionQuizz[count].reponses.map((data, index) => (
-                        <div key={index}>
-                            <input type="radio" id={index} name="quizz" value={data.reponse} />
-                            <label htmlFor={index} > {data.reponse} </label>
-                        </div>
-                    ))
-            }
-            <button id="submit" onClick={()=> validation()}>Valider</button>
+            <div id="QuizzEnCours">
+                <h2>Score: {score} </h2>
+                {
+                        QuestionQuizz[count].reponses.map((data, index) => (
+                            <div key={index}>
+                                <input type="radio" id={index} name="quizz" value={data.reponse} />
+                                <label htmlFor={index} > {data.reponse} </label>
+                            </div>
+                        ))
+                }
+                <button id="submit" onClick={()=> validation()}>Valider</button>
+            </div>
+            <div id="QuizzTermine">
+                <h2>Le Quizz est terminé</h2>
+                <p>Vous avez eu {score}/10</p>
+                <button onClick={()=> relancer()}>Rejouer</button>
+            </div>
         </section>
     )
 };
