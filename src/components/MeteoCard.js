fcacf2dea05ac8a28ae9a94c6be0a4bac4b71ce3
Alexandre <caramel.alexandre@gmail.com>
2022-02-06 13:33:23 +0100

    ADD real data for meteoCard

--- src/components/MeteoCard.js
+++ src/components/MeteoCard.js
@@ -1,11 +1,17 @@
 import '../Sass/components/_MeteoCard.scss';
 
-const MeteoCard = ({ville, temp, icon}) => {
+const MeteoCard = ({temp, name, icon}) => {
 
+    const iconImg = 'http://openweathermap.org/img/wn/'+ icon +'@2x.png'
+    console.log(iconImg);
     return ( 
         <div id='MeteoCard'>
             <div>
-                <h2>{ville}</h2>
+                <h2>{name}</h2>
+                <div>
+                    <h3>{temp} °C</h3>
+                    <img src={iconImg} alt="WeatherCondition" />
+                </div>
                 
             </div>
         </div>
--- src/components/MeteoComponent.js
+++ src/components/MeteoComponent.js
@@ -8,28 +8,34 @@ const MeteoComponent = () => {
     const [data, setData] = useState('');
     const [ville, setVille]= useState('paris');
 
-    console.log(ville);
-
     useEffect(() => {
         axios.get(
             'https://api.openweathermap.org/data/2.5/weather?q='+ville+'&appid=ea47585d5d7af6136536556b8101c20d&units=metric&lang=fr'
             )
-        .then((res) => setData(res.data));
-        console.log(data);
+        .then((res) => {
+            setData(res.data)
+        });
+        
     }, [ville]);
 
     function submit(){
         const InputVille = document.getElementById('villeinput').value
         setVille(InputVille)
     }
-    
 
     return ( 
         <div id='MeteoComponent'>
             <div>
                 <input type="text" id='villeinput' />
                 <input type="submit" value="Valider" onClick={()=>submit()}/>
-                <MeteoCard ville={data.name} temp={data.main.temp} icon={data.weather.icon}  />
+                {(()=> {
+                    if(data !== ''){
+                        return(
+                            <MeteoCard temp={data.main.temp} name={data.name} icon={data.weather[0].icon} />
+                        )
+                    }   
+                })()}
+                
             </div>
         </div>
     );
